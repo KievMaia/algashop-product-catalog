527df8efbdb9f821762ac2f84fcd7ce5b34daccc
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2026-06-05 18:04:22 -0300

    Criando os testes de contrato.

---
+++ src/contractTest/resources/contracts/product/findProductByIdV1.groovy
@@ -0,0 +1,34 @@
+package contracts.product
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method GET()
+        headers {
+            accept 'application/json'
+        }
+        url("/api/v1/products/019dbc11-088b-7476-8dc0-6cf690b8624b")
+    }
+    response {
+        status 200
+        headers {
+            contentType'application/json'
+        }
+        body([
+                id: fromRequest().path(3),
+                addedAt: anyIso8601WithOffset(),
+                name: "Notebook X11",
+                brand: "Deep Driver",
+                regularPrice: 1500.00,
+                salePrice: 1000.0,
+                inStock: true,
+                enable: true,
+                category: [
+                        id:  anyUuid(),
+                        name: "Notebook"
+                ],
+                description: "A Gamer Notebook"
+        ])
+    }
+}
---
+++ src/main/java/com/algaworks/algashop/product_catalog/presentation/ProductController.java
@@ -0,0 +1,58 @@
+package com.algaworks.algashop.product_catalog.presentation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDetailOutput crete(@RequestBody ProductInput input) {
+        return ProductDetailOutput.builder()
+                .id(UUID.randomUUID())
+                .addedAt(OffsetDateTime.now())
+                .inStock(false)
+                .name(input.getName())
+                .brand(input.getBrand())
+                .description(input.getDescription())
+                .regularPrice(input.getRegularPrice())
+                .salePrice(input.getSalePrice())
+                .enabled(input.getEnabled())
+                .category(CategoryMinimalOutput.builder()
+                        .id(input.getCategoryId())
+                        .name("Notebook")
+                        .build())
+                .build();
+    }
+
+    @GetMapping("/{productId}")
+    public ProductDetailOutput findById(@PathVariable("productId") UUID productId) {
+        return ProductDetailOutput.builder()
+                .id(productId)
+                .addedAt(OffsetDateTime.now())
+                .name("Notebook X11")
+                .brand("Deep Driver")
+                .regularPrice(new BigDecimal("1500.00"))
+                .salePrice(new BigDecimal("1000.00"))
+                .inStock(true)
+                .enabled(true)
+                .category(CategoryMinimalOutput.builder()
+                        .id(UUID.randomUUID())
+                        .name("Notebook")
+                        .build())
+                .description("A Gamer Notebook")
+                .build();
+    }
+}
